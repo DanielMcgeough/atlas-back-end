462670a74c318cb3aae5f3555272fa4e0ec2f0ec
DanielMcgeough <daniel.mcgeough@atlasschool.com>
2024-07-11 13:57:42 -0500

    reworked.

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -1,26 +1,32 @@
 #!/usr/bin/python3
 """
-Exports the to do list info for a
-employee determined by the employee id
-and then exports the data to a JSON
-file
+Exports to-do list information for a given employee ID to JSON format.
+
+This script takes an employee ID as a command-line argument and exports
+the corresponding user information and to-do list to a JSON file.
 """
 
 import json
 import requests
 import sys
 
+
 if __name__ == "__main__":
+    # Get the employee ID from the command-line argument
     user_id = sys.argv[1]
 
-    url = "http://jsonplaceholder.typicode.com/"
+    # Base URL for the JSONPlaceholder API
+    url = "https://jsonplaceholder.typicode.com/"
 
-    user = requests.get(url + "users/{}".format(user_id)), json()
+    # Fetch user information using the provided employee ID
+    user = requests.get(url + "users/{}".format(user_id)).json()
     username = user.get("username")
 
+    # Fetch the to-do list for the employee using the provided employee ID
     params = {"userId": user_id}
-    todos = requests.get(url + "todos")
+    todos = requests.get(url + "todos", params).json()
 
+    # Create a dictionary containing the user and to-do list information
     data_to_export = {
         user_id: [
             {
@@ -31,5 +37,7 @@ if __name__ == "__main__":
             for t in todos
         ]
     }
+
+    # Write the data to a JSON file with the employee ID as the filename
     with open("{}.json".format(user_id), "w") as jsonfile:
         json.dump(data_to_export, jsonfile, indent=4)
