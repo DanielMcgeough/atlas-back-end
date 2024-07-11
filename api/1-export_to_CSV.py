bc7c2efa79316d8c2ae48900af455ec4602d9626
DanielMcgeough <daniel.mcgeough@atlasschool.com>
2024-07-11 12:14:25 -0500

    made it pretty.

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -6,28 +6,28 @@ import requests
 import sys
 
 if __name__ == "__main__":
-    # Get the user ID from the command-line arguments provided to the script
-    user_id = sys.argv[1]
+    # Get the employee ID from the command-line arguments provided to the script
+    employee_id = sys.argv[1]
 
     # Define the base URL for the JSON API
     url = "https://jsonplaceholder.typicode.com/"
 
-    # Fetch user information from the API and
+    # Fetch employee information from the API and
     #   convert the response to a JSON object
-    user = requests.get(url + "users/{}".format(user_id)).json()
+    employee = requests.get(url + "employees/{}".format(employee_id)).json()
 
-    # Extract the username from the user data
-    username = user.get("username")
+    # Extract the employeename from the employee data
+    employeename = employee.get("employeename")
 
     # Fetch the to-do list items associated with the
-    #   given user ID and convert the response to a JSON object
-    todos = requests.get(url + "todos", params={"userId": user_id}).json()
+    #   given employee ID and convert the response to a JSON object
+    todos = requests.get(url + "todos", params={"employeeId": employee_id}).json()
 
     # Use list comprehension to iterate over the to-do list items
-    # Write each item's details (user ID, username, completion status,
+    # Write each item's details (employee ID, employeename, completion status,
     #   and title) as a row in the CSV file
-    with open("{}.csv".format(user_id), "w", newline="") as csvfile:
+    with open("{}.csv".format(employee_id), "w", newline="") as csvfile:
         writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
         [writer.writerow(
-            [user_id, username, t.get("completed"), t.get("title")]
+            [employee_id, employeename, t.get("completed"), t.get("title")]
          ) for t in todos]
